4dfed3ef48e7e70791ab9093ea269f7486e00846
zhuotuo <zhuotuo@leagsoft.com>
2023-03-31 11:50:38 +0800

    'test'

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
+/*
+ * @Author: zhuotuo
+ * @Date: 2023-03-30 08:38:22
+ * @LastEditors: zhuotuo
+ * @LastEditTime: 2023-03-30 14:41:25
+ * @Description: 
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/drag-demo/',
-  outputDir: 'docs',
-  assetsDir: 'drag-demo'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/drag-demo/'
+    : '/'
 })
